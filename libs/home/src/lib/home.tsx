90a0064982093e85c168fad23821597a2e203fb5
Behemoth <simonostini@gmail.com>
2021-10-07 21:31:49 +0200

    setup

---
+++ libs/home/src/lib/home.tsx
@@ -0,0 +1,14 @@
+import './home.module.scss';
+
+/* eslint-disable-next-line */
+export interface HomeProps {}
+
+export const Home = (props: HomeProps) => {
+  return (
+    <div>
+      <h1>Alps BTE</h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ libs/router/src/lib/router.tsx
@@ -0,0 +1,14 @@
+import './router.module.scss';
+
+/* eslint-disable-next-line */
+export interface RouterProps {}
+
+export function Router(props: RouterProps) {
+  return (
+    <div>
+      <h1>Welcome to Router!</h1>
+    </div>
+  );
+}
+
+export default Router;
