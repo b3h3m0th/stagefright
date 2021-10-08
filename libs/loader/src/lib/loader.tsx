bd923e4ed0d1e0d557fd4ec45fc7370ddd68a2c2
Behemoth <simonostini@gmail.com>
2021-10-08 09:09:37 +0200

    update

---
+++ libs/loader/src/lib/loader.tsx
@@ -0,0 +1,14 @@
+import './loader.module.scss';
+
+/* eslint-disable-next-line */
+export interface LoaderProps {}
+
+export const Loader = (props: LoaderProps) => {
+  return (
+    <div>
+      <h1>Loading</h1>
+    </div>
+  );
+};
+
+export default Loader;
