e11776b677acb6b49769bac66b316e869d9c118e
Behemoth <simonostini@gmail.com>
2021-10-08 07:44:53 +0200

    update

--- apps/stagefright/src/app/app.spec.tsx
+++ apps/stagefright/src/app/app.spec.tsx
@@ -1,5 +1,4 @@
-import { cleanup, getByText, render, waitFor } from '@testing-library/react';
-import React from 'react';
+import { render } from '@testing-library/react';
 import App from './app';
 
 describe('App', () => {
