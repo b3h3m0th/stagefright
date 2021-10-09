7a946274602a6080cd1597a938720ae556228a4d
Behemoth <simonostini@gmail.com>
2021-10-09 17:03:57 +0200

    update

---
+++ libs/shared/components/src/lib/footer/footer.tsx
@@ -0,0 +1,14 @@
+import './footer.scss';
+
+/* eslint-disable-next-line */
+export interface FooterProps {}
+
+export const Footer: React.FC = (props: FooterProps) => {
+  return (
+    <div>
+      <h1>Welcome to Footer!</h1>
+    </div>
+  );
+};
+
+export default Footer;
