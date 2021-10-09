ef38e5430989bcb1c4f41dd242ca70c7e4a555b2
Behemoth <simonostini@gmail.com>
2021-10-09 19:26:17 +0200

    update

--- libs/home/src/lib/components/shows/shows.tsx
+++ libs/home/src/lib/components/shows/shows.tsx
@@ -1,20 +1,18 @@
 import './shows.scss';
-// import { Helmet } from 'react-helmet';
 import { shows, IShow } from '@stagefright/shared/data';
 
 /* eslint-disable-next-line */
 export interface ShowsProps {}
 
 export const Shows: React.FC = (props: ShowsProps) => {
-  // useScript('https://widget.bandsintown.com/main.min.js');
+  const upcomingShows = shows.filter((show) => new Date() < show.start);
 
   return (
     <div className="shows" id="shows">
       <h1 className="shows__title">Upcoming Shows</h1>
       <div className="shows__content">
-        {shows
-          .filter((show) => new Date() < show.start)
-          .map((show: IShow) => (
+        {upcomingShows.length > 0 ? (
+          upcomingShows.map((show: IShow) => (
             <div className="shows__content__show">
               <div className="shows__content__show__date">
                 <p className="shows__content__show__date__day">
@@ -57,37 +55,10 @@ export const Shows: React.FC = (props: ShowsProps) => {
                 )}
               </div>
             </div>
-          ))}
-        {/* <Helmet>
-          <script
-            charSet="utf-8"
-            src="https://widget.bandsintown.com/main.min.js"
-          ></script>
-          <a
-            className="bit-widget-initializer"
-            data-artist-name="StageFright Official"
-            data-display-past-dates="true"
-            data-text-color="#000000"
-            data-link-color="#830000"
-            data-background-color="#ffffff"
-            data-display-details="true"
-            data-popup-background-color="#ffffff"
-            data-link-text-color="#FFFFFF"
-            data-separator-color="#CBCBCB"
-            data-language="en"
-            data-font="Helvetica"
-            data-display-local-dates="false"
-            data-auto-style="false"
-            data-display-lineup="false"
-            data-display-play-my-city="true"
-            data-display-limit="15"
-            data-display-start-time="false"
-            href="https://google.com"
-          >
-            a
-          </a>
-        </Helmet> */}
-        {/* <iframe src="https://bnds.us/3bze0r" title="StageFright Shows"></iframe> */}
+          ))
+        ) : (
+          <p className="shows__content__empty">Coming Soon!</p>
+        )}
       </div>
     </div>
   );
