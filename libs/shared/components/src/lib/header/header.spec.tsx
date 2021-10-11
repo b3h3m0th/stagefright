e44f7b93ae71c8d98c50a1c6445111bdcd85b8b9
Behemoth <simonostini@gmail.com>
2021-10-11 09:31:52 +0200

    fix test

--- libs/shared/components/src/lib/header/header.spec.tsx
+++ libs/shared/components/src/lib/header/header.spec.tsx
@@ -1,10 +1,15 @@
 import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 
 import Header from './header';
 
 describe('Header', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Header />);
+    const { baseElement } = render(
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
+    );
     expect(baseElement).toBeTruthy();
   });
 });
