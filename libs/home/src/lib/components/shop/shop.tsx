2bcd48001f2db618a9e8438cab234a3b351bd592
Behemoth <simonostini@gmail.com>
2021-10-18 14:39:07 +0200

    updated title type

--- libs/home/src/lib/components/shop/shop.tsx
+++ libs/home/src/lib/components/shop/shop.tsx
@@ -6,7 +6,7 @@ export interface ShopProps {}
 export const Shop: React.FC = (props: ShopProps) => {
   return (
     <div className="shop">
-      <h1 className="shop__title">Shop</h1>
+      <h2 className="shop__title">Shop</h2>
     </div>
   );
 };
