6d6df095289f21aa415092fc62abf697eb672390
Behemoth <simonostini@gmail.com>
2021-11-07 18:12:04 +0100

    fixed responsiveness

--- libs/home/src/lib/components/hero/hero.tsx
+++ libs/home/src/lib/components/hero/hero.tsx
@@ -1,3 +1,5 @@
+import { breakpoints } from '@stagefright/shared/config';
+import { useWindowSize } from '@stagefright/shared/util';
 import React, { useEffect, useState } from 'react';
 import './hero.scss';
 
@@ -6,25 +8,32 @@ export interface HeroProps {}
 
 export const Hero: React.FC<HeroProps> = (props: HeroProps) => {
   const [offset, setOffset] = useState<number>(0);
+  const { width: windowWidth } = useWindowSize();
 
   /*eslint-disable-next-line */
-  const offsetScroll: () => void = () => {
-    setOffset(window.scrollY);
-  };
+  const offsetScroll: () => void =
+    windowWidth >= breakpoints.phone
+      ? () => setOffset(window.scrollY)
+      : () => void 0;
 
   useEffect(() => {
     window.addEventListener('scroll', offsetScroll);
     return () => {
       window.removeEventListener('scroll', offsetScroll);
     };
-  });
+  }, [windowWidth, offsetScroll]);
 
   return (
     <div className="hero">
       <div className="hero__content">
         <h1
           className="hero__content__headline"
-          style={{ transform: `translate(-${offset}px, 44px)` }}
+          style={{
+            transform:
+              windowWidth >= breakpoints.phone
+                ? `translate(-${offset}px, 44px)`
+                : '',
+          }}
         >
           Debut Album{' '}
           <span className="hero__content__headline__album-title">Wanted</span>{' '}
