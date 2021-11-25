67e6e64fcb3a75df7dcb37fe1e8c824050660543
Behemoth <simonostini@gmail.com>
2021-11-25 08:50:04 +0100

    add strong types

--- libs/shared/util/src/lib/hooks/useFirstRender.ts
+++ libs/shared/util/src/lib/hooks/useFirstRender.ts
@@ -1,7 +1,7 @@
 import { useRef } from 'react';
 
 export function useFirstRender(): boolean {
-  const ref = useRef(true);
+  const ref = useRef<boolean>(true);
   const firstRender = ref.current;
   ref.current = false;
   return firstRender;
