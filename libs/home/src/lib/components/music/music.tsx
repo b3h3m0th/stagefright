9a0714390ba08da55cbd274344f0d061490c57c1
Behemoth <simonostini@gmail.com>
2021-11-26 09:50:26 +0100

    added webgl shaders

--- libs/home/src/lib/components/music/music.tsx
+++ libs/home/src/lib/components/music/music.tsx
@@ -1,3 +1,4 @@
+//@flow
 /*eslint-disable-next-line */
 import { HomeSection } from '@stagefright/router';
 import './music.scss';
@@ -8,7 +9,37 @@ import { musicData, IMusic, animationData } from '@stagefright/shared/config';
 import { useMousePosition } from '@stagefright/shared/util';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpotify, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { GLSL, Shaders, Node } from 'gl-react';
+import { Surface } from 'gl-react-dom';
 gsap.registerPlugin(ScrollTrigger);
+
+const shaders = Shaders.create({
+  helloGL: {
+    // This is our first fragment shader in GLSL language (OpenGL Shading Language)
+    // (GLSL code gets compiled and run on the GPU)
+    frag: GLSL`
+          precision highp float;
+          varying vec2 uv;
+          void main() {
+            gl_FragColor = vec4(uv.x, uv.y, 0.5, 1.0);
+          }
+      `,
+    // the main() function is called FOR EACH PIXELS
+    // the varying uv is a vec2 where x and y respectively varying from 0.0 to 1.0.
+    // we set in output the pixel color using the vec4(r,g,b,a) format.
+    // see GLSL_ES_Specification_1.0.17
+  },
+});
+
+const Shader: React.FC = () => {
+  return (
+    <Surface width={350} height={400}>
+      <Node shader={shaders.helloGL} />
+    </Surface>
+  );
+  // Surface creates the canvas, an area of pixels where you can draw.
+  // Node instanciates a "shader program" with the fragment shader defined above.
+};
 
 /* eslint-disable-next-line */
 export interface MusicProps {}
@@ -60,10 +91,6 @@ export const Music: MemoExoticComponent<React.FC<MusicProps>> = memo(
       };
     });
 
-    useEffect(() => {
-      void 0;
-    }, []);
-
     return (
       <section className="music" id={HomeSection.music}>
         <h2 className="music__title" ref={titleRef}>
@@ -79,6 +106,7 @@ export const Music: MemoExoticComponent<React.FC<MusicProps>> = memo(
               ref={followImageRef}
               src={musicData[0].cover}
             />
+            <Shader />
           </div>
           {musicData.map((m: IMusic, i: number) => {
             return (
