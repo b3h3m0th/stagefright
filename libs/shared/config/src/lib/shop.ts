3afb0aa98b5ec59ba0bd1d015f59a4293d518cc1
Behemoth <simonostini@gmail.com>
2021-11-26 18:06:13 +0100

    update

--- libs/shared/config/src/lib/shop.ts
+++ libs/shared/config/src/lib/shop.ts
@@ -15,28 +15,28 @@ export const shopData = <const>{
         image:
           'https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
         altText: `${altTextPrefix} Shirt Size S`,
-        status: 'Out of Stock',
+        status: 'Available Soon',
       },
       {
         name: 'Shirt M',
         image:
           'https://images.unsplash.com/photo-1490481651871-ab68de25d43d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
         altText: `${altTextPrefix} Shirt Size M`,
-        status: 'Out of Stock',
+        status: 'Available Soon',
       },
       {
         name: 'Shirt L',
         image:
           'https://images.unsplash.com/photo-1469334031218-e382a71b716b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
         altText: `${altTextPrefix} Shirt Size L`,
-        status: 'Out of Stock',
+        status: 'Available Soon',
       },
       {
         name: 'Shirt XL',
         image:
           'https://images.unsplash.com/photo-1496747611176-843222e1e57c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZmFzaGlvbnxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60',
         altText: `${altTextPrefix} Shirt Size XL`,
-        status: 'Out of Stock',
+        status: 'Available Soon',
       },
     ],
     sticker: [
@@ -45,7 +45,7 @@ export const shopData = <const>{
         image:
           'https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
         altText: `${altTextPrefix} Vintage Sticker`,
-        status: 'Available Soon',
+        status: 'Out of Stock',
       },
     ],
   },
