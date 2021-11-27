411dde721c8ec896a62d9ed7928ee798f2908259
Behemoth <simonostini@gmail.com>
2021-11-27 17:57:58 +0100

    fix

--- libs/shared/util/src/lib/window/scrollToHash.ts
+++ libs/shared/util/src/lib/window/scrollToHash.ts
@@ -2,7 +2,7 @@ export function scrollToHash(): void {
   const id: string = window.location.hash.substr(1);
 
   if (id) {
-    const anchor: HTMLElement = document.getElementById(id);
+    const anchor: HTMLElement | null = document.getElementById(id);
     anchor && anchor.scrollIntoView();
   }
 }
