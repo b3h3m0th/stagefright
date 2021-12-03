d721fe1355cdc1526bd8c4b4e59ed739760b4030
Behemoth <simonostini@gmail.com>
2021-12-03 20:52:01 +0100

    update

--- libs/shared/config/src/lib/music.ts
+++ libs/shared/config/src/lib/music.ts
@@ -35,4 +35,12 @@ export const musicData: Musics = [
       'https://images.unsplash.com/photo-1496747611176-843222e1e57c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZmFzaGlvbnxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60',
     releaseDate: new Date(2021, 11, 31),
   },
+  {
+    title: 'Walk',
+    subtitle: 'StageFright Single',
+    type: 'Single',
+    cover:
+      'https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    releaseDate: new Date(2021, 11, 31),
+  },
 ];
