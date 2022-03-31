ed27dcd9cffff2ed086db751fed313d6cd3afcc1
Behemoth <simonostini@gmail.com>
2022-03-31 15:30:24 +0200

    band text update

--- libs/home/src/lib/components/band/band.tsx
+++ libs/home/src/lib/components/band/band.tsx
@@ -4,16 +4,32 @@ import { SectionTitle } from '@stagefright/shared/components';
 import { bandBio } from '@stagefright/shared/data';
 import './band.scss';
 import band from '../../../../../../apps/stagefright/src/assets/img/band_hero.png';
+import { useEffect } from 'react';
+import { animationData, loading } from '@stagefright/shared/config';
+import gsap from 'gsap';
 
 /* eslint-disable-next-line */
 export interface BandProps {}
 
 export const Band: React.FC = (props: BandProps) => {
+  useEffect(() => {
+    gsap.from('.band__content__band-bio', {
+      delay: loading.artificialPageMountDelay / 1000,
+      opacity: 0,
+      duration: animationData.shows.duration / 1000,
+      scrollTrigger: {
+        trigger: '.band__content__band-bio',
+        toggleActions: 'play none none none',
+        start: 'top 90%',
+      },
+    });
+  });
+
   return (
     <div id={HomeSection.band} className="band">
       <SectionTitle text="Band" />
       <div className="band__content">
-        <img src={band} alt="StageFright band" />
+        {/* <img src={band} alt="StageFright band" /> */}
         <div className="band__content__band-bio">{bandBio}</div>
       </div>
     </div>
--- libs/shared/data/src/lib/bandbio.ts
+++ libs/shared/data/src/lib/bandbio.ts
@@ -1,5 +1,5 @@
 export const bandBio = `
-StageFright ist eine 2018 gegründete, österreichische Hard Rock Band aus Vorarlberg. Nach mehreren Line-Up-Changes entstand die jetzige fünfköpfige Konstellation aus Sänger Andy, Rhythmusgitarrist Ace, Leadgitarrist Skip, Bassist Brandon und Drummer Wolf.
-In den Folgejahren war StageFright bei zahlreichen wichtigen Events und Konzerten zu sehen wie beispielsweise dem Szene Open Air 2019. Seit der Gründung der Band spielt StageFright ununterbrochen Live Shows.
-Im Sommer 2021 begann die Band mit den Studioaufnahmen des Debut Albums „Wanted“ bei den Nautilus Sound Studios. Das, nach dem Titeltrack benannte, Album besteht aus insgesamt 12 Nummern und lässt sich als klassisches Hard Rock Werk mit verschiedenen Elementen des modernen Metals beschreiben und wird im Laufe dieses Jahres veröffentlicht werden.
+StageFright is a hard rock band from Vorarlberg, Austria, founded in 2018. After several line-up changes, the current five-piece constellation of singer Andy, rhythm guitarist Ace, lead guitarist Skip, bassist Brandon and drummer Wolf was formed.
+In the following years, StageFright performed at numerous major events and concerts such as Szene Open Air 2019. Since the band's inception, StageFright has been playing live shows non-stop.
+In the summer of 2021, the band began recording their debut album "Wanted" at Nautilus Sound Studios.  The album, named after the title track, consists of 12 numbers in total and can be described as a classic hard rock work with various elements of modern metal and will be released later this year.
 ` as const;
